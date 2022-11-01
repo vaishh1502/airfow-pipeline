9c660b02a47cfe3623ca40e61e9acb50baa6d0ff
unknown <Administrator@BG4NB0945.SONATA.LOCAL>
2022-11-01 11:44:11 +0530

    updated

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 yum update -y
+yum install -y https://s3.eu-central-1.amazonaws.com/amazon-ssm-eu-central-1/latest/linux_amd64/amazon-ssm-agent.rpm
+systemctl start amazon-ssm-agent
+systemctl status amazon-ssm-agent
